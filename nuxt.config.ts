e7bd68cf103655084d90461614c9af6696448c60
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-08-19 10:55:14 +0900

    feat: microCMSの準備

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
-import { defineNuxtConfig } from 'nuxt'
+export default {
+  // 略
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({
-
-})
+  buildModules: ["nuxt-microcms-module"],
+  microcms: {
+    options: {
+      serviceDomain: "service-domain",
+      apiKey: "2fc820ea0e8643e4978f81750916ad5b09e6",
+    },
+    mode: process.env.NODE_ENV === "production" ? "server" : "all",
+  },
+};
