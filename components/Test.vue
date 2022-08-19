e3936538a24f36a18ea8143e30e9ac184ff10ec6
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-08-19 10:40:46 +0900

    feat: 環境構築

---
+++ components/Test.vue
@@ -0,0 +1,5 @@
+<template>
+<div>
+    testComponent
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+home
+  </div>
+  <Test />
+</template>
+
+<script setup>
+useHead({
+  title: 'My App',
+  // or, instead:
+  // titleTemplate: (title) => `My App - ${title}`,
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+  charset: 'utf-8',
+  meta: [
+    { name: 'description', content: 'My amazing site.' }
+  ],
+  bodyAttrs: {
+    class: 'test'
+  }
+})
+</script>
