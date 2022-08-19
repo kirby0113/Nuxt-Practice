ca523f30dad6c55d6a4ea364497dfc9d772b7cdb
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-08-19 13:37:42 +0900

    feat: fetchテスト

--- pages/test/index.vue
+++ pages/test/index.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <ul>
+      <li v-for="article in data.contents" :key="article.id">
+        <nuxt-link :to="`/${article.id}`">{{ article.title }}</nuxt-link>
+      </li>
+    </ul>
   </div>
 </template>
+
+<script setup>
+const ctx = useRuntimeConfig();
+
+const { data } = await useFetch("/tasks", {
+  baseURL: `https://${ctx.serviceDomain}.microcms.io/api/v1`,
+  headers: {
+    "X-MICROCMS-API-KEY": ctx.apiKey,
+  },
+})
+</script>
