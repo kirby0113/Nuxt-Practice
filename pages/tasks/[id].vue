76958207b992891b44f85db15814803fe33cb4fe
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-08-19 14:02:45 +0900

    feat: タスク詳細ページ作成

---
+++ pages/tasks/[id].vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+  タスク詳細ページ
+</div>
+  <div>
+    タスク名：{{data.title}}
+  </div>
+    <div>
+    タスク期限：{{data.limit}}
+  </div>
+  <div>
+    <div>
+      タスク詳細
+    </div>
+  <div>{{data.detail}}</div>
+    </div>
+</template>
+
+<script setup>
+const ctx = useRuntimeConfig();
+const { id } = useRoute().params
+
+const { data } = await useFetch(`/tasks/${id}`, {
+  baseURL: `https://${ctx.serviceDomain}.microcms.io/api/v1`,
+  headers: {
+    "X-MICROCMS-API-KEY": ctx.apiKey,
+  },
+})
+console.log(data);
+</script>
